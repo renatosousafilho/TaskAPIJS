0f738e0620e1265a4a7b350632c0eba397259700
Renato Filho <renatosousafilho@gmail.com>
2021-02-24 12:22:55 -0300

    deixando nosso codigo mais expressivo

--- schemas/TaskSchema.js
+++ schemas/TaskSchema.js
@@ -7,16 +7,24 @@ const errors = {
     code: 422, 
     message: 'Nome deve ter pelo menos 3 caracteres' 
   },
+  name_string: {
+    code:  422,
+    message: "Deveria ser uma string"
+  },
   blank_user: { 
     code: 422, 
     message: 'Usuário deve ser preenchido' 
   },
   user_min_length: { 
     code: 422, 
-    message: 'Usuário deve ter pelo menos 3 caracteres' 
+    message: 'Usuário deve ter pelo menos 5 caracteres' 
   },
   no_error: {}
 }
+
+const blank = (value) => (!value);
+const isNotString  = (value) => (typeof value !== 'string')
+const isNotMinLength = (value, length) => (value.length < length);
 
 module.exports = (name, user) => {
   const MIN_LENGTH = 3;
@@ -27,11 +35,14 @@ module.exports = (name, user) => {
 
   // return errors.no_error;
 
+  console.log(typeof name);
+
   switch (true) {
-    case (!name): return errors.blank_name;
-    case (name.length < MIN_LENGTH): return errors.name_min_length;
-    case (!user): return errors.blank_user;
-    case (user.length < MIN_LENGTH): return errors.user_min_length;
+    case (blank(name)): return errors.blank_name;
+    case (isNotString(name)): return errors.name_string;
+    case (isNotMinLength(name, 3)): return errors.name_min_length;
+    // case (required(user)): return errors.blank_user;
+    // case (min(user, 5)): return errors.user_min_length;
     default: return errors.no_error;
   }
 };
