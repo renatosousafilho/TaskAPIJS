b30c5810e5af988066e6da24bfea3920eff1edcd
Renato Filho <renatosousafilho@gmail.com>
2021-02-24 13:24:15 -0300

    deixando nosso codigo mais expressivo

--- schemas/TaskSchema.js
+++ schemas/TaskSchema.js
@@ -24,25 +24,17 @@ const errors = {
 
 const blank = (value) => (!value);
 const isNotString  = (value) => (typeof value !== 'string')
-const isNotMinLength = (value, length) => (value.length < length);
+const lengthLessThan = (value, length) => (value.length < length);
 
 module.exports = (name, user) => {
   const MIN_LENGTH = 3;
-  // if (!name) errors.blank_name;
-  // if (name.length < MIN_LENGTH) return errors.name_min_length;
-  // if (!user) return errors.blank_user;
-  // if (user.length < MIN_LENGTH) return errors.user_min_length;
-
-  // return errors.no_error;
-
-  console.log(typeof name);
-
+  
   switch (true) {
-    case (blank(name)): return errors.blank_name;
-    case (isNotString(name)): return errors.name_string;
-    case (isNotMinLength(name, 3)): return errors.name_min_length;
-    // case (required(user)): return errors.blank_user;
-    // case (min(user, 5)): return errors.user_min_length;
+    case blank(name): return errors.blank_name;
+    case isNotString(name): return errors.name_string;
+    case lengthLessThan(name, 3): return errors.name_min_length;
+    case blank(user): return errors.blank_user;
+    case lengthLessThan(user, 5): return errors.user_min_length;
     default: return errors.no_error;
   }
 };
